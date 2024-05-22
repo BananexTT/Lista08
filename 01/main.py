31709d384470bc64ea04c495da06dbf044e2ed86
BananexTT <bananareidelass@gmail.com>
2024-05-21 21:48:53 -0300

    Aula 21/05

---
+++ 01/main.py
@@ -0,0 +1,8 @@
+from funções import *
+
+num = int(input("Informe um número inteiro: "))
+
+absoluto = obter_valor_absoluto(num)
+
+print (f"O valor absoluto de {num} é {absoluto}")
+
---
+++ 02/funcoes.py
@@ -0,0 +1,2 @@
+def obter_maior(x: int, y:int) -> int:
+    return x if x > y else y
